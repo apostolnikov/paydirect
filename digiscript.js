f50a99be586168fea9724bcf3d844ba879fb3847
Apostol Nikov <apostolnikov@users.noreply.github.com>
2022-04-21 20:30:56 +0300

    add digiscript.js

---
+++ digiscript.js
@@ -0,0 +1,13 @@
+let cartButton = document.querySelector(".cart__checkout-button");
+if (cartButton) {
+  cartButton.addEventListener("click", (e) => {
+    //fetch cart obj
+    fetch("/cart.js")
+      .then((res) => res.json())
+      .then((cartObj) => {
+        console.log("FABY  ", { cartObj });
+        window.location.replace("https://cartapp.digitalcart.app/#e8a153371b");
+      })
+      .catch((err) => console.log(err));
+  });
+}
